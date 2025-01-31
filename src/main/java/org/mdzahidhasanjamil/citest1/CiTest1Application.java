e449f54b79a9a40daaf7413669abe29463e751c1
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2025-01-31 10:36:32 +0600

    formating with spotless

--- src/main/java/org/mdzahidhasanjamil/citest1/CiTest1Application.java
+++ src/main/java/org/mdzahidhasanjamil/citest1/CiTest1Application.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CiTest1Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CiTest1Application.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(CiTest1Application.class, args);
+  }
 }
--- src/test/java/org/mdzahidhasanjamil/citest1/controller/UserControllerTest.java
+++ src/test/java/org/mdzahidhasanjamil/citest1/controller/UserControllerTest.java
@@ -1,26 +1,26 @@
 package org.mdzahidhasanjamil.citest1.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired private MockMvc mockMvc;
 
-    @Test
-    public void test() throws Exception {
-        mockMvc.perform(get("/user/test"))
-                .andExpect(status().isOk()) // Check if the status is 200 OK
-                .andExpect(content().string("Test successfully done with updated version")); // Verify response body
-    }
+  @Test
+  public void test() throws Exception {
+    mockMvc
+        .perform(get("/user/test"))
+        .andExpect(status().isOk()) // Check if the status is 200 OK
+        .andExpect(
+            content()
+                .string("Test successfully done with updated version")); // Verify response body
+  }
 }
