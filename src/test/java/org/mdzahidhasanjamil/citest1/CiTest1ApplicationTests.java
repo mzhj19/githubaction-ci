9c85c42b21fb463564b841f63d35b412aa2a59b7
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2025-02-08 14:28:07 +0600

    sonarqube cloud

--- src/test/java/org/mdzahidhasanjamil/citest1/CiTest1ApplicationTests.java
+++ src/test/java/org/mdzahidhasanjamil/citest1/CiTest1ApplicationTests.java
@@ -1,8 +1,6 @@
 package org.mdzahidhasanjamil.citest1;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CiTest1ApplicationTests {
-}
+class CiTest1ApplicationTests {}
